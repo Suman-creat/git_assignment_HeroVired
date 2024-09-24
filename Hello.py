79153aa426a5bfda021dd2a8e7b4a3fb1fc0abf0
unknown <suman2840contai@gmail.com>
2024-09-24 20:30:04 +0530

    addition

---
+++ Hello.py
@@ -0,0 +1,3 @@
+name=input('what is your name? ')
+colour=input(' what is your fevourite colour')
+print(name +" favourite colour is " +colour)
---
+++ addition.py
@@ -0,0 +1,8 @@
+a=2
+n=4
+def add():
+    print(a +n)
+
+add()
+
+
